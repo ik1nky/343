8d0a8b821dc0f3b312ee573159c0c17a38283cbd
Jack S. Rosenhauer II <jackrosenhauer@gmail.com>
2014-10-23 13:33:49 -0400

    Pushed my infix2postfix.c file

--- infix2postfix.c
+++ infix2postfix.c
@@ -8,34 +8,54 @@
 #include "stack.h"
 #include "infix2postfix.h"
 
+/*http://www.youtube.com/watch?v=vq-nUF0G4fI*/
 /* function to convert an infix to postfix */
+/*    ((A+B))*C-D)*E -> AB+C*D-E*     */
+
+stack ops;
+
 char *infixToPostfix(char *infixStr)
 {
+	char *str = malloc(10 * sizeof(char));
+	/*tokenize it*/
+	stackInit(&ops);
+	char * token;
+	token = strtok(infixStr," ");
+
+	char op;
+
+	while (token != NULL){
+		/*#skip operand, we can use last case as an else*/
+		if (isLeftParen(token)){
+
+		}else if (isOperand(token)){
+
+		}else if (isOperator(token)){
+
+		}else if(isRightParen(token)){
+
+		}else{
+			//it's a number
+
+		}
+
+		sprintf(str, "%c", num);
+		stackPush(&ops, str);
+
+		printf("%s", token);
+		token = strtok(NULL," ");
+	}
+
 	return NULL;
 }
 
 /* function that returns true if the string is an operator */
 bool isOperator(char *str)
 {
-	switch (str)
-	{
-		case '+':
-			return true;
-		case '-':
-			return true;
-		case '*':
-			return true;
-		case '/':
-			return true;
-		case '%':
-			return true;
-		case '^':
-			return true;
-                case '(':
-                        return true;
-		default:
-			return false;
+	if (*str == '*' || *str == '/' || *str == '%' || *str == '^' || *str == '+' || *str == '-'){
+		return true;
 	}
+	return false;
 }
 
 /* function that returns true if the string is an operand/integer */
@@ -47,31 +67,68 @@ bool isOperand(char *str)
 /* function that returns true if the string is a left parenthesis */
 bool isLeftParen(char *str)
 {
+	if (*str == ')'){
+		return true;
+	}
 	return false;
 }
 
 /* function that returns true if the string is a right parenthesis */
 bool isRightParen(char *str)
 {
+	if (*str == '('){
+		return true;
+	}
 	return false;
 }
 
 /* function that returns the stack precedence of given operator */
 int stackPrecedence(char *str)
 {
-	return 0;
+	switch (*str){
+		case '+':
+			return 1;
+		case '-':
+			return 1;
+		case '*':
+			return 2;
+		case '/':
+			return 2;
+		case '%':
+			return 2;
+		case '^':
+			return 3;
+		default:
+			return 1;
+	}
 }
 
 /* function that returns the input precedence of given operator */
 int inputPrecedence(char *str)
 {
-	return 0;
+	switch (*str){
+		case '+':
+			return 1;
+		case '-':
+			return 1;
+		case '*':
+			return 2;
+		case '/':
+			return 2;
+		case '%':
+			return 2;
+		case '^':
+			return 4;
+		default:
+			/* value of '(' */
+			return 5;
+	}
 }
 
 /* function that evaluates a postfix expression and returns the result */
 int evaluatePostfix(char *postfixStr)
 {
-	return 0;
+	return 69;
 }
 
 /* apply operator to num1 and num2 and return the result */
@@ -79,4 +136,3 @@ int applyOperator(int num1, int num2, char *opr)
 {
 	return 0;
 }
-
